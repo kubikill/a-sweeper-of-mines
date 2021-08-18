b0fe31d1be657bbeaac2f2ae6f6606ca65394af9
Jakub Topoliński <topolinskijakub5@gmail.com>
2021-08-18 20:39:04 +0200

    Updates

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function (event) {
           "./icons.1894e4dd.css",
           "./icons.3d969a31.ttf",
           "./normalize.50fa89bf.css",
-          "./custom.ba776df4.css",
+          "./custom.c2b56dca.css",
           "./index.b829ac74.js",
         ]);
       }),
